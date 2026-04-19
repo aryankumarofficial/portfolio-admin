feac151fa6deb284a8854a43cafe399a3f2c36f8
Aryan Kumar <aryanak9163@gmail.com>
2026-04-20 04:23:22 +0530

    feat: adds the favicon

---
+++ app/icon.tsx
@@ -0,0 +1,45 @@
+import { ImageResponse } from "next/og"
+
+export const size = {
+  width: 32,
+  height: 32,
+}
+
+export const contentType = "image/png"
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#22c55e",
+          borderRadius: 4,
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <rect width="20" height="14" x="2" y="7" rx="2" ry="2" />
+          <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+        </svg>
+      </div>
+    ),
+    {
+      width: size.width,
+      height: size.height,
+    }
+  )
+}
